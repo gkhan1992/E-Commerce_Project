84b519e28ae39f246ad8a2360530770436532a52
gkhan1992 <98851571+gkhan1992@users.noreply.github.com>
2022-06-10 20:12:07 +0300

    Generic Repository Design Pattern Project

---
+++ Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+	public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
+	{
+		IQueryable<T> GetAll(); //Hangi türdeysek(örnek:product) getall fonksiyonunu çağırınca bütün productları getir(sorgu için ıqueryable)
+		IQueryable<T> GetWhere(Expression<Func<T, bool>> method); //sorguya uygun olanları getirir çoğul
+		Task <T> GetSingleAsync(Expression<Func<T, bool>> method); //sorguya uygun ilk olanı getirir tekil
+		Task <T> GetByIdAsync(string id); //ıd ye uygun olanı getirir
+	}
+}
---
+++ Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,22 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+	public interface IWriteRepository<T> : IRepository<T> where T : BaseEntity
+	{
+		Task<bool> AddAsync(T model); //gelen modeli ekle
+		Task<bool> AddRangeAsync(List<T> datas); //birden fazla koleksiyonu ekle
+		bool Remove(T model);
+		bool RemoveRange(List<T> datas);
+		Task <bool> RemoveAsync (string id);
+		bool Update(T model);
+
+		Task<int> SaveAsync();
+
+	}
+}
